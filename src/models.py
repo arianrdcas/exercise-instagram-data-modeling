915bd62fb5c34dc3ac9fc7f71508a4eaaf49025d
arianrdcas <arian4geeka@gmail.com>
2022-05-17 20:30:47 +0000

    prueba

--- src/models.py
+++ src/models.py
@@ -8,23 +8,67 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'users'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    user_name = Column(String(250), nullable=False, unique=True)
+    pass_user = Column(String(250), nullable=False)
+    email_user = Column(String(250), nullable=False, unique=True)
+    url_user = Column(String(250), nullable=False)
+    presentacion = Column(String(250), nullable=False)
+    foto_user = Column(String(250))
+    
 
-class Address(Base):
-    __tablename__ = 'address'
+class Follower(Base):
+    __tablename__ = 'followers'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    id_user = Column(Integer,ForeignKey('user.id'))  
+    id_user_follower = Column(Integer,ForeignKey('user.id'))
+
+
+
+class Following(Base):
+    __tablename__ = 'followings'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    id_user = Column(Integer,ForeignKey('user.id'))  
+    id_user_following = Column(Integer,ForeignKey('user.id'))
+
+
+
+class Post(Base):
+    __tablename__ = 'posts'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    id_user_post = Column(Integer,ForeignKey('user.id'))
+    id_tipo_post = Column(Integer,ForeignKey('tipo_post.id'))
+    descripcion = Column(String(250), nullable=False)
+    like = Column(Integer, nullable=False)
+
+
+class Tipo_Post(Base):
+    __tablename__ = 'potipo_posts'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    tipo_post = Column(String(250), nullable=False)
+
+
+
+class Comment(Base):
+    __tablename__ = 'comments'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    id_post = Column(Integer,ForeignKey('post.id'))
+    descripcion = Column(String(250), nullable=False)
+
 
     def to_dict(self):
         return {}
